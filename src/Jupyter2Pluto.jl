f53ddb9df0abfcc906c94adba12dea79289513bb
adrhill <adrian.hill@mailbox.org>
2023-02-16 21:17:50 +0100

    Add first draft

--- src/Jupyter2Pluto.jl
+++ src/Jupyter2Pluto.jl
@@ -1,5 +1,24 @@
 module Jupyter2Pluto
 
-# Write your package code here.
+using JSON 
+using UUIDs: uuid1
+using Pluto: Cell, Notebook, save_notebook   
+export jupyter2pluto
 
+function convert_cell(cell::Dict)
+    type = get(cell, "cell_type", "missing")
+    source = join(get(cell, "source", ""))
+    type == "code" && return Cell(source)
+    type == "markdown" && return Cell("md\"\"\"\n$(source)\n\"\"\"")
+    error("Unknown cell type $(type)")
 end
+
+function jupyter2pluto(input_path, output_path)
+    jnb = open(JSON.parse, input_path, "r")
+    cells = convert_cell.(jnb["cells"])
+    pnb = Notebook(cells, output_path, uuid1())
+    
+    save_notebook(pnb, output_path)
+    println("Pluto notebook has been saved to $(output_path).")
+end
+end # module
---
+++ test/assets/test_output.jl
@@ -0,0 +1,21 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 98b02fe6-ae35-11ed-0cf0-35c10272f444
+md"""
+# Notebook title
+## Header
+
+Markdown content with an equation: $x=1$
+"""
+
+# ╔═╡ 98b03042-ae35-11ed-2ed0-2fce17570ede
+# a simple code cell
+foo = 1
+
+# ╔═╡ Cell order:
+# ╠═98b02fe6-ae35-11ed-0cf0-35c10272f444
+# ╠═98b03042-ae35-11ed-2ed0-2fce17570ede
