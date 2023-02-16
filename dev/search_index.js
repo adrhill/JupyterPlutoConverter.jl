fda572809ef582be98816ba68e231b1dd26dbf01
Documenter.jl <documenter@juliadocs.github.io>
2023-02-16 20:25:45 +0000

    build based on cb4bc93

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Jupyter2Pluto","category":"page"},{"location":"#Jupyter2Pluto","page":"Home","title":"Jupyter2Pluto","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Jupyter2Pluto.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Jupyter2Pluto]","category":"page"}]
+}
