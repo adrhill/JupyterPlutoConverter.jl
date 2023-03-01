2c024d610241f02b127a018eff4aeddc38e992b1
Adrian Hill <adrian.hill@mailbox.org>
2023-03-02 00:25:24 +0100

    Refactor using `walkdir` (#4)

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,8 +10,8 @@ function notebooks_are_equal(out_path, ref_path)
 end
 
 @testset "JupyterPlutoConverter.jl" begin
-    ref_folder = joinpath(@__DIR__, "references")
-    input_path = joinpath(ref_folder, "input.ipynb")
+    ref_dir = joinpath(@__DIR__, "references")
+    input_path = joinpath(ref_dir, "input.ipynb")
 
     # Run tests with different keyword arguments
     test_cases = Dict(
@@ -22,11 +22,19 @@ end
 
     @testset "Files" begin
         for (test_name, kwargs) in test_cases
+            # These tests write a Pluto notebook to a temporary file
+            # that will be deleted at the end of the test
             @testset "$(test_name)" begin
-                # Write Pluto notebook to a temporary file
-                # that will be deleted at the end of the test
-                tmp_path = joinpath(ref_folder, "_tmp_$(test_name).jl")
-                ref_path = joinpath(ref_folder, "output_$(test_name).jl")
+
+                # Default output path
+                @test_logs (:info,) jupyter2pluto(input_path; kwargs...)
+                default_output_path = replace(input_path, ".ipynb" => ".jl")
+                ref_path = joinpath(ref_dir, "output_$(test_name).jl")
+                @test notebooks_are_equal(default_output_path, ref_path)
+                rm(default_output_path)
+
+                # Specify output path
+                tmp_path = joinpath(ref_dir, "tmp_$(test_name).jl")
                 @test_logs (:info,) jupyter2pluto(input_path, tmp_path; kwargs...)
                 @test notebooks_are_equal(tmp_path, ref_path)
                 rm(tmp_path)
@@ -34,7 +42,7 @@ end
         end
     end
     @testset "Overwrite protection" begin
-        output_path = joinpath(ref_folder, "dummy_file.jl")
+        output_path = joinpath(ref_dir, "dummy_file.jl")
         file_content = "Lorem ipsum"
         open(output_path, "w") do io
             write(io, file_content)
@@ -46,29 +54,38 @@ end
         @test only(readlines(output_path)) == file_content
         # # This will overwrite the file:
         @test_logs (:info,) jupyter2pluto(input_path, output_path; overwrite=true)
-        @test notebooks_are_equal(output_path, joinpath(ref_folder, "output_default.jl"))
+        @test notebooks_are_equal(output_path, joinpath(ref_dir, "output_default.jl"))
         rm(output_path)
     end
     @testset "Directories" begin
         # File that should be written:
         output_path = joinpath(@__DIR__, "references", "input.jl")
-        for (test_name, kwargs) in test_cases
-            @testset "Recursive $(test_name)" begin
-                @test_logs (:info,) jupyter2pluto("."; recursive=true, kwargs...)
-                @test isfile(output_path)
-                ref_path = joinpath(ref_folder, "output_$(test_name).jl")
-                @test notebooks_are_equal(output_path, ref_path)
-                rm(output_path)
+        @testset "Recursive" begin
+            for (test_name, kwargs) in test_cases
+                @testset "$test_name" begin
+                    @test_logs (:info,) jupyter2pluto("."; recursive=true, kwargs...)
+                    @test isfile(output_path)
+                    ref_path = joinpath(ref_dir, "output_$(test_name).jl")
+                    @test notebooks_are_equal(output_path, ref_path)
+                    rm(output_path)
+                end
             end
         end
         @testset "Non-recursive" begin
-            jupyter2pluto("."; recursive=false)
+            @test_logs (:warn,) jupyter2pluto("."; recursive=false)
             @test !isfile(output_path)
             # If something went wrong and file got created, delete it
             isfile(output_path) && rm(output_path)
         end
+        @testset "Empty dir" begin
+            @test_logs (:warn,) jupyter2pluto(
+                joinpath(ref_dir, "empty_dir"); recursive=false
+            )
+        end
     end
+
     @testset "Error cases" begin
-        @test_throws ErrorException jupyter2pluto("runtests.jl")
+        @test_throws ErrorException jupyter2pluto("runtests.jl") # not .ipynb
+        @test_throws ErrorException jupyter2pluto("foo.ipynb") # doesn't exist
     end
 end
