4842747499f6fa1c1a1c1ff898c80f35b9118a33
Fons van der Plas <fonsvdplas@gmail.com>
2025-08-01 14:07:20 +0200

    Allow returning multiple Strings from `transform_code` to create multiple cells (#10)

---
+++ test/references/output_split_cells.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.19.47
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 8cda7c94-6e1d-11f0-3418-5d1e68bef28b
+md"""
+# Test Notebook
+## Test Header
+
+This notebook is used to test JupyterPlutoConverter.jl.
+This is Markdown content with an equation: $x=1$
+"""
+
+# ╔═╡ 8ce07928-6e1d-11f0-0496-a58229fcb216
+foo = 1 # a simple code cell
+
+# ╔═╡ 8ce1937e-6e1d-11f0-0eff-b54351c2e832
+foo = 1 # a simple code cell
+
+# ╔═╡ 8ce19434-6e1d-11f0-32b4-67a11081c30a
+md"The following code cell is used to test wrapping code in `begin ... end` blocks:"
+
+# ╔═╡ Cell order:
+# ╟─8cda7c94-6e1d-11f0-3418-5d1e68bef28b
+# ╠═8ce07928-6e1d-11f0-0496-a58229fcb216
+# ╠═8ce1937e-6e1d-11f0-0eff-b54351c2e832
+# ╟─8ce19434-6e1d-11f0-32b4-67a11081c30a
